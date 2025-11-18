0fccb139615e73edfa3966f8a82ed64c5c6460e4
englishbasictobrilliance <englishbasictobrilliance@gmail.com>
2025-11-18 11:49:32 +0000

    ...

---
+++ routes/StudentCourseRoutes.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__ . '/../controllers/StudentCourseController.php';
+
+$pdo = $GLOBALS['pdo'];
+$studentCourseController = new StudentCourseController($pdo);
+
+return [
+    // Create a new student course entry - Private (JWT)
+    'POST /student-courses/' => [
+        'handler' => function () use ($studentCourseController) {
+            $studentCourseController->createRecord();
+        },
+        'auth' => 'private'
+    ],
+
+    // Get all student course entries - Private (JWT)
+    'GET /student-courses/' => [
+        'handler' => function () use ($studentCourseController) {
+            $studentCourseController->getAllRecords();
+        },
+        'auth' => 'private'
+    ],
+
+    // Get a student course entry by ID - Private (JWT)
+    'GET /student-courses/{id}/' => [
+        'handler' => function ($id) use ($studentCourseController) {
+            $studentCourseController->getRecordById($id);
+        },
+        'auth' => 'private'
+    ],
+
+    // Update a student course entry - Private (JWT)
+    'PUT /student-courses/{id}/' => [
+        'handler' => function ($id) use ($studentCourseController) {
+            $studentCourseController->updateRecord($id);
+        },
+        'auth' => 'private'
+    ],
+
+    // Delete a student course entry - Private (JWT)
+    'DELETE /student-courses/{id}/' => [
+        'handler' => function ($id) use ($studentCourseController) {
+            $studentCourseController->deleteRecord($id);
+        },
+        'auth' => 'private'
+    ],
+];
