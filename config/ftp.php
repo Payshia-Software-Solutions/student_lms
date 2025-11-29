a327fb64f16c3cd18baa9f145e973ef691138a67
englishbasictobrilliance <englishbasictobrilliance@gmail.com>
2025-11-29 20:21:35 +0000

    change the course post format

--- config/ftp.php
+++ config/ftp.php
@@ -1,10 +1,7 @@
 <?php
 
-// FTP Configuration
 return [
-    'server' => 'ftp.payshia.com', // <-- Replace with your FTP server
-    'user' => 'student-lms@payshia.com',
+    'server' => 'ftp.payshia.com',
+    'username' => 'student-lms@payshia.com',
     'password' => 'f8XudlE97x%nx;^.',
-    'root_path' => '/', // The base directory for all uploads. Can be changed to '/httpdocs/uploads' etc.
-    'public_url' => 'https://student-lms-ftp.payshia.com/', // The base public URL of your website
 ];
